099360c1a65fdb3c6c901e5911ac93f4b0e996b6
LuckDev <lukedru@yahoo.com>
2023-02-21 10:28:51 -0800

    Initial Commit

---
+++ TicClock/TicClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicClockApp.swift
+//  TicClock
+//
+//  Created by Luke Drushell on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct TicClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
